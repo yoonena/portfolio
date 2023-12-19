3f165e2ed257b4e483fa5971e306caba7fd50994
ena <yoonyoonena@gmail.com>
2023-12-19 17:30:56 +0900

    update

---
+++ common/js/default.js
@@ -0,0 +1,28 @@
+var responsiveSwiper = new Swiper(".swiper", {
+	slidesPerView: 2,
+	slidesPerColumn: 2,
+	spaceBetween: 10,			
+	loop: true,
+	autoplay: { 
+		delay: 3000,
+		disableOnInteraction: false, 
+	},			
+	pagination: {
+		el: '.swiper-pagination',
+		clickable: true,
+	},
+	navigation: {
+		nextEl: ".swiper-button-next",
+		prevEl: ".swiper-button-prev",
+	},
+	breakpoints:{
+		769: {
+			slidesPerView: 3,
+			slidesPerColumn: 3,
+		},
+		1201: {
+			slidesPerView: 4,
+			slidesPerColumn: 4,
+		}
+	}
+});
