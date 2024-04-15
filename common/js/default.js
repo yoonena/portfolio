a9d18406ff36fb9bc3b28aa7f496bfc3625dc5ce
ena <yoonyoonena@gmail.com>
2024-04-15 18:16:31 +0900

    update for details

--- common/js/default.js
+++ common/js/default.js
@@ -1,4 +1,4 @@
-var responsiveSwiper = new Swiper(".swiper", {
+var responsiveSwiper = new Swiper(".swiper_2024", {
 	slidesPerView: 2,
 	slidesPerColumn: 2,
 	spaceBetween: 10,			
@@ -8,12 +8,76 @@ var responsiveSwiper = new Swiper(".swiper", {
 		disableOnInteraction: false, 
 	},			
 	pagination: {
-		el: '.swiper-pagination',
+		el: '.swiper-pagination_2024',
 		clickable: true,
 	},
 	navigation: {
-		nextEl: ".swiper-button-next",
-		prevEl: ".swiper-button-prev",
+		nextEl: ".swiper-button-next_2024",
+		prevEl: ".swiper-button-prev_2024",
+	},
+	breakpoints:{
+		769: {
+			slidesPerView: 3,
+			slidesPerColumn: 3,
+		},
+		1201: {
+			slidesPerView: 4,
+			slidesPerColumn: 4,
+		}
+	},
+	touchEventsTarget: 'wrapper',
+	touchRatio: 0.5, 
+    touchAngle: 45, 
+});
+
+var responsiveSwiper = new Swiper(".swiper_2023", {
+	slidesPerView: 2,
+	slidesPerColumn: 2,
+	spaceBetween: 10,			
+	loop: true,
+	autoplay: { 
+		delay: 3000,
+		disableOnInteraction: false, 
+	},			
+	pagination: {
+		el: '.swiper-pagination_2023',
+		clickable: true,
+	},
+	navigation: {
+		nextEl: ".swiper-button-next_2023",
+		prevEl: ".swiper-button-prev_2023",
+	},
+	breakpoints:{
+		769: {
+			slidesPerView: 3,
+			slidesPerColumn: 3,
+		},
+		1201: {
+			slidesPerView: 4,
+			slidesPerColumn: 4,
+		}
+	},
+	touchEventsTarget: 'wrapper',
+	touchRatio: 0.5, 
+    touchAngle: 45, 
+});
+
+var responsiveSwiper = new Swiper(".swiper_2022", {
+	slidesPerView: 2,
+	slidesPerColumn: 2,
+	spaceBetween: 10,			
+	loop: true,
+	autoplay: { 
+		delay: 3000,
+		disableOnInteraction: false, 
+	},			
+	pagination: {
+		el: '.swiper-pagination_2022',
+		clickable: true,
+	},
+	navigation: {
+		nextEl: ".swiper-button-next_2022",
+		prevEl: ".swiper-button-prev_2022",
 	},
 	breakpoints:{
 		769: {
