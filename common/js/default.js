cbf85858a5a40cdc130593ebc500a94d7eabf899
ena <yoonyoonena@gmail.com>
2024-06-21 14:03:30 +0900

    last work

--- common/js/default.js
+++ common/js/default.js
@@ -2,19 +2,19 @@ var responsiveSwiper1 = new Swiper(".swiper_2024", {
 	slidesPerView: 2,
 	slidesPerColumn: 1,
 	spaceBetween: 10,			
-	loop: true,
+	// loop: true,
 	autoplay: { 
 		delay: 3000,
 		disableOnInteraction: false, 
 	},			
-	pagination: {
-		el: '.swiper-pagination_2024',
-		clickable: true,
-	},
-	// navigation: {
-	// 	nextEl: ".swiper-button-next_2024",
-	// 	prevEl: ".swiper-button-prev_2024",
+	// pagination: {
+	// 	el: '.swiper-pagination_2024',
+	// 	clickable: true,
 	// },
+	navigation: {
+		nextEl: ".swiper-button-next_2024",
+		prevEl: ".swiper-button-prev_2024",
+	},
 	breakpoints:{
 		769: {
 			slidesPerView: 3,
@@ -32,19 +32,19 @@ var responsiveSwiper2 = new Swiper(".swiper_2023", {
 	slidesPerView: 2,
 	slidesPerColumn: 1,
 	spaceBetween: 10,			
-	loop: true,
+	// loop: true,
 	autoplay: { 
 		delay: 3000,
 		disableOnInteraction: false, 
 	},			
-	pagination: {
-		el: '.swiper-pagination_2023',
-		clickable: true,
-	},
-	// navigation: {
-	// 	nextEl: ".swiper-button-next_2023",
-	// 	prevEl: ".swiper-button-prev_2023",
+	// pagination: {
+	// 	el: '.swiper-pagination_2023',
+	// 	clickable: true,
 	// },
+	navigation: {
+		nextEl: ".swiper-button-next_2023",
+		prevEl: ".swiper-button-prev_2023",
+	},
 	breakpoints:{
 		769: {
 			slidesPerView: 3,
@@ -62,19 +62,19 @@ var responsiveSwiper3 = new Swiper(".swiper_2022", {
 	slidesPerView: 2,
 	slidesPerColumn: 1,
 	spaceBetween: 10,			
-	loop: true,
+	// loop: true,
 	autoplay: { 
 		delay: 3000,
 		disableOnInteraction: false, 
 	},			
-	pagination: {
-		el: '.swiper-pagination_2022',
-		clickable: true,
-	},
-	// navigation: {
-	// 	nextEl: ".swiper-button-next_2022",
-	// 	prevEl: ".swiper-button-prev_2022",
+	// pagination: {
+	// 	el: '.swiper-pagination_2022',
+	// 	clickable: true,
 	// },
+	navigation: {
+		nextEl: ".swiper-button-next_2022",
+		prevEl: ".swiper-button-prev_2022",
+	},
 	breakpoints:{
 		769: {
 			slidesPerView: 3,
